f31e9be2c3dc38c38bb69085d277c7857ae1001b
Amir <sh.hossein67@gmail.com>
2021-07-14 15:12:37 +0430

    add include/ChessBoard file to prj, this file has empty class

---
+++ include/ChessBoard.hpp
@@ -0,0 +1,11 @@
+#ifndef CHESSBOARD_H
+#define CHESSBOARD_H
+
+#include "Cell.hpp"
+
+class ChessBoard // This class used Singeleton pattern
+{
+
+};
+
+#endif /* end of include guard: CHESSBOARD_H*/
