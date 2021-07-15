c73d7b5140cb65b66e75fac2b92300bd78d359fc
mahdijamebozorg <mahdijamebozorg@email.com>
2021-07-15 09:56:22 -0400

    object files

--- moc_backend.cpp
+++ moc_backend.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_BackEnd_t {
-    QByteArrayData data[5];
-    char stringdata0[30];
+    QByteArrayData data[11];
+    char stringdata0[85];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,13 +33,21 @@ struct qt_meta_stringdata_BackEnd_t {
 static const qt_meta_stringdata_BackEnd_t qt_meta_stringdata_BackEnd = {
     {
 QT_MOC_LITERAL(0, 0, 7), // "BackEnd"
-QT_MOC_LITERAL(1, 8, 7), // "getIcon"
-QT_MOC_LITERAL(2, 16, 0), // ""
-QT_MOC_LITERAL(3, 17, 6), // "choose"
-QT_MOC_LITERAL(4, 24, 5) // "index"
+QT_MOC_LITERAL(1, 8, 9), // "getP1Name"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 6), // "P1Name"
+QT_MOC_LITERAL(4, 26, 9), // "getP2Name"
+QT_MOC_LITERAL(5, 36, 6), // "P2Name"
+QT_MOC_LITERAL(6, 43, 11), // "getGameName"
+QT_MOC_LITERAL(7, 55, 8), // "GameName"
+QT_MOC_LITERAL(8, 64, 7), // "getIcon"
+QT_MOC_LITERAL(9, 72, 6), // "choose"
+QT_MOC_LITERAL(10, 79, 5) // "index"
 
     },
-    "BackEnd\0getIcon\0\0choose\0index"
+    "BackEnd\0getP1Name\0\0P1Name\0getP2Name\0"
+    "P2Name\0getGameName\0GameName\0getIcon\0"
+    "choose\0index"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +57,7 @@ static const uint qt_meta_data_BackEnd[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,12 +65,18 @@ static const uint qt_meta_data_BackEnd[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x0a /* Public */,
-       3,    1,   25,    2, 0x0a /* Public */,
+       1,    1,   39,    2, 0x0a /* Public */,
+       4,    1,   42,    2, 0x0a /* Public */,
+       6,    1,   45,    2, 0x0a /* Public */,
+       8,    0,   48,    2, 0x0a /* Public */,
+       9,    1,   49,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void, QMetaType::QString,    7,
     QMetaType::QString,
-    QMetaType::Void, QMetaType::UInt,    4,
+    QMetaType::Void, QMetaType::UInt,   10,
 
        0        // eod
 };
@@ -73,9 +87,12 @@ void BackEnd::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         auto *_t = static_cast<BackEnd *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: { QString _r = _t->getIcon();
+        case 0: _t->getP1Name((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->getP2Name((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->getGameName((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 3: { QString _r = _t->getIcon();
             if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = std::move(_r); }  break;
-        case 1: _t->choose((*reinterpret_cast< uint(*)>(_a[1]))); break;
+        case 4: _t->choose((*reinterpret_cast< uint(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -110,13 +127,13 @@ int BackEnd::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 5;
     }
     return _id;
 }
