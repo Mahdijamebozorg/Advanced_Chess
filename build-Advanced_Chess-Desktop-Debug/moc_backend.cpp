129fb40e331d115d377e5875468ee5c2dd201b43
mahdijamebozorg <mahdijamebozorg@email.com>
2021-07-15 07:55:32 -0400

    add build directory to main folder

---
+++ build-Advanced_Chess-Desktop-Debug/moc_backend.cpp
@@ -0,0 +1,124 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'backend.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../temp/Advanced_Chess/backend.hpp"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'backend.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.13.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_BackEnd_t {
+    QByteArrayData data[5];
+    char stringdata0[30];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_BackEnd_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_BackEnd_t qt_meta_stringdata_BackEnd = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "BackEnd"
+QT_MOC_LITERAL(1, 8, 7), // "getIcon"
+QT_MOC_LITERAL(2, 16, 0), // ""
+QT_MOC_LITERAL(3, 17, 6), // "choose"
+QT_MOC_LITERAL(4, 24, 5) // "index"
+
+    },
+    "BackEnd\0getIcon\0\0choose\0index"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_BackEnd[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    1,   25,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::QString,
+    QMetaType::Void, QMetaType::UInt,    4,
+
+       0        // eod
+};
+
+void BackEnd::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<BackEnd *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: { QString _r = _t->getIcon();
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = std::move(_r); }  break;
+        case 1: _t->choose((*reinterpret_cast< uint(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject BackEnd::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_BackEnd.data,
+    qt_meta_data_BackEnd,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *BackEnd::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *BackEnd::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_BackEnd.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int BackEnd::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
