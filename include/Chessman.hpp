904b49a97d843b4a61ec4c2368ad73066d818453
Amir <sh.hossein67@gmail.com>
2021-08-03 10:46:13 +0430

    add abstract Chessman Class header and implementions file: header/Chessman.hpp src/Chessman.cpp this

--- include/Chessman.hpp
+++ include/Chessman.hpp
@@ -1,10 +1,51 @@
 #ifndef CHESSMAN_H
 #define CHESSMAN_H
 
-// abstract class 
+#include <string>
+#include <list>
+
 class Chessman
 {
+public:
+  // Type alias
+  using ID        = std::string                                   ;
+  using Index     = std::pair<unsigned int, unsigned int>         ;
+  using Icon      = std::string                                   ;
 
+  // declare enum
+  enum ChessType {PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING}; // for prevent from many dynamic_cast
+  enum Color     {WHITE, BLACK}                           ;
+
+  // Constructor
+  Chessman(Color           );
+  Chessman(const Chessman &); // Copy constructor
+
+  // set and get functions
+          ChessType getChessType(     ) const    ;
+          void      setColor    (Color)          ;
+          Color     getColor    (     ) const    ;
+  virtual void      setID       (     )       = 0;
+          ID        getID       (     ) const    ;
+          Icon      getIcon     (     ) const    ;
+  virtual void      setIcon     (Icon )       = 0;
+
+  // moves functinos
+  virtual std::list<Index> getCellsCanGo(Index) const = 0;
+
+  // Destructor
+  virtual ~Chessman();
+
+
+protected:
+  // protected data member
+  ChessType chess_type;
+  ID        id        ;
+  Icon      icon      ;
+
+
+private:
+  // Data member
+  Color color;
 
 };
 
---
+++ src/Chessman.cpp
@@ -0,0 +1,46 @@
+#include "../Header/Chessman.hpp"
+
+#include <stdexcept>
+
+using namespace std;
+
+extern void checkRange(Chessman::Index);
+
+
+Chessman::Chessman(Color color)
+{
+  setColor(color);
+}
+
+Chessman::Chessman(const Chessman & chessman)
+{
+  setColor(chessman.getColor());
+}
+
+Chessman::~Chessman()
+{}
+
+Chessman::ChessType Chessman::getChessType() const
+{
+  return chess_type;
+}
+
+void Chessman::setColor(Color color)
+{
+  this->color = color;
+}
+
+Chessman::Color Chessman::getColor() const
+{
+  return color;
+}
+
+Chessman::ID Chessman::getID() const
+{
+  return id;
+}
+
+Chessman::Icon Chessman::getIcon() const
+{
+  return icon;
+}
