bb88220bb1c0f7f0bfe9a3d05c26282088d09258
Amir <sh.hossein67@gmail.com>
2021-08-03 12:11:21 +0430

    add Queen class to prj, this class inherate from Chessman class. file: include/Queen.hpp src/Queen.cpp

--- include/Queen.hpp
+++ include/Queen.hpp
@@ -1,11 +1,28 @@
-#ifndef Queen_H
-#define Queen_H
+#ifndef QUEEN_H
+#define QUEEN_H
 
 #include "Chessman.hpp"
 
 class Queen : public Chessman
 {
+public:
+  // Constructor
+  Queen(Color, Icon = "");
+  Queen(const Queen&    ); // Copy constructor
 
+  // moves functinos
+  virtual std::list<Index> getCellsCanGo(Index) const;
+
+  // Set public functions
+  virtual void setIcon(Icon);
+
+
+private:
+  // Data member
+  static size_t cnt;
+
+  // set functinos
+  virtual void setID();
 };
 
-#endif /* end of include guard: Queen_H*/
+#endif /* end of include guard: QUEEN_H*/
---
+++ src/Queen.cpp
@@ -0,0 +1,104 @@
+#include "../Header/Queen.hpp"
+
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+extern void checkRange(Chessman::Index);
+
+
+size_t Queen::cnt = 1;
+
+Queen::Queen(Color color, Icon icon):Chessman(color)
+{
+  this->chess_type = QUEEN;
+  setID  (    );
+  setIcon(icon);
+}
+
+Queen::Queen(const Queen & queen):Chessman(queen)
+{
+  this->chess_type = QUEEN;
+  setID  (    );
+  setIcon(icon);
+}
+
+void Queen::setID()
+{
+  Chessman::ID temp = "Q";
+
+  if(getColor() == WHITE)
+    temp += "W";
+  else
+    temp += "B";
+
+  temp += to_string(cnt++);
+
+  this->id = temp;
+}
+
+list<Queen::Index> Queen::getCellsCanGo(Index index) const
+{
+  checkRange(index); // Checked range index
+
+  Index temp;
+  list<Index> l;
+
+  enum {RIGHT = 0, LEFT, UP, DOWN};
+  enum {UP_RIGHT = 4, UP_LEFT, DOWN_RIGHT, DOWN_LEFT};
+  for(int i = 0; i < 8; i++)
+  {
+    temp = index;
+
+    bool in_range = true;
+    while(in_range)
+    {
+      if(i < 4) // if not cross move
+      {
+        // first right chessman push in list so left so up and so down delimeter
+        // delimeter is pair that first and second equal 100
+        if(i == RIGHT)  // 0:Right 1:Left 2:UP 3:Down
+          temp.second++;
+        else if(i == LEFT)
+          temp.second--;
+        else if(i == UP)
+          temp.first--;
+        else if(i == DOWN)
+          temp.first++;
+      }
+      else // if cross move
+      {
+        // first North-East and so North-West and so South-East so Douth-West
+        // delimeter is pair both is 100
+        if(i == UP_RIGHT || i == UP_LEFT) // 4:North-East 5:North-West 6:South-East 7:South-West
+          temp.first--;
+        else if( i == DOWN_RIGHT || i == DOWN_LEFT)
+          temp.first++;
+
+        if(i == UP_RIGHT || i == DOWN_RIGHT)
+          temp.second++;
+        else if( i == UP_LEFT || i == DOWN_LEFT)
+          temp.second--;
+      }
+
+      try
+      {
+        checkRange(temp);
+      }
+      catch(invalid_argument& w)
+      {
+        in_range = false;
+        temp = Index(100, 100);
+      }
+
+      l.push_back(temp);
+    }
+  }
+
+  return l;
+}
+
+void Queen::setIcon(Icon icon)
+{
+  this->icon = icon;
+}
