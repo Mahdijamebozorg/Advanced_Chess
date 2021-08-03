39199ef2cfe0a8d08cacf0be0ad0879da6ad39a2
Amir <sh.hossein67@gmail.com>
2021-08-03 11:34:22 +0430

    add Class Rook to prj, this class inheriate from Chessman class file: include/Rook.hpp src/Rook.cpp

---
+++ src/Rook.cpp
@@ -0,0 +1,105 @@
+#include "../Header/Rook.hpp"
+
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+extern void checkRange(Chessman::Index);
+
+
+size_t Rook::cnt = 1;
+
+Rook::Rook(Color color, Icon icon):Chessman(color)
+{
+  this->chess_type = ROOK;
+  setID  (    );
+  setIcon(icon);
+}
+
+Rook::Rook(const Rook & rook):Chessman(rook)
+{
+  this->chess_type = ROOK;
+  setID  (    );
+  setIcon(icon);
+}
+
+void Rook::setID()
+{
+  Chessman::ID temp = "R";
+
+  if(getColor() == WHITE)
+    temp += "W";
+  else
+    temp += "B";
+
+  temp += to_string(cnt++);
+
+  this->id = temp;
+}
+
+list<Rook::Index> Rook::getCellsCanGo(Index index) const
+{
+  checkRange(index); // Checked range index
+
+  Index temp;
+  list<Index> l;
+
+  enum {RIGHT = 0, LEFT, UP, DOWN};
+  // first right chessman push in list so left so up and so down delimeter
+  // delimeter is pair that first and second equal 100
+  for(int i = 0; i < 4; i++)
+  {
+    temp = index;
+
+    bool in_range = true;
+    while(in_range)
+    {
+      if(i == RIGHT)
+        temp.second++;
+
+      else if(i == LEFT)
+        temp.second--;
+
+      else if(i == UP)
+        temp.first--;
+
+      else if(i == DOWN)
+        temp.first++;
+
+      try
+      {
+        checkRange(temp);
+      }
+      catch(invalid_argument& w)
+      {
+        in_range = false;
+        temp = Index(100, 100);
+      }
+
+      l.push_back(temp);
+    }
+  }
+
+  return l;
+}
+
+void Rook::setIcon(Icon icon)
+{
+  this->icon = icon;
+}
+
+void Rook::rookMoved()
+{
+  this->moved = true;
+}
+
+bool Rook::getMoved() const
+{
+  return moved;
+}
+
+void Rook::rookNotMoved()
+{
+ moved = false;
+}
