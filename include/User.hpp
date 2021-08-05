aace6130381e418530184f7bb76d411f381429e1
Amir <sh.hossein67@gmail.com>
2021-08-05 20:06:46 +0430

    add User class to prj, this class actually is user in this chess game. this class used from Singeleton pattern, so can at least two instance create from this class file: include/User.hpp src/User.cpp

---
+++ include/User.hpp
@@ -0,0 +1,80 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "Chessman.hpp"
+
+class User // we can only two create object this class
+{
+public:
+  using Color          = Chessman::Color                       ;
+  using Name           = std::string                           ;
+  using Score          = int                                   ;
+  using ChessmansIn    = std::vector<std::shared_ptr<Chessman>>;
+  using ChessmansOut   = std::vector<std::shared_ptr<Chessman>>;
+  using PawnsReachLast = std::vector<std::shared_ptr<Chessman>>;
+
+  // get and set functions for make Singleton pattern
+  static User*& get(Name, Color, Score positive_score = 0, Score negative_score = 0);
+
+  // set and get functions
+  void  setName         (Name )      ;
+  Name  getName         (     ) const;
+  void  setColor        (Color)      ;
+  Color getColor        (     ) const;
+  void  setScore        (Score)      ;
+  Score getScore        (     ) const;
+  Score incScore        (Score)      ;
+  Score decScore        (Score)      ;
+  void  setNegativeScore(Score)      ;
+  Score getNegativeScore(     ) const;
+  Score incNegativeScore(Score)      ;
+
+  // initialize chessmans for start game
+  void                      setChessmansIn (                     )      ;
+  ChessmansIn               getChessmansIn (                     ) const;
+  ChessmansOut              getChessmansOut(                     ) const;
+  std::shared_ptr<Chessman> getChessman    (Chessman::ID, bool in) const;
+
+  // hit chessman
+  void                      hitChessman       (Chessman::ID, bool chessman_pawn_final_home = false); // Hit chessman
+  std::shared_ptr<Chessman> backChessmanInGame(Chessman::ID                                       ); // Undo Chessman in game
+  void                      addToChessmansIn  (std::shared_ptr<Chessman>                          ); // add chessman to ChessmanIn
+  std::shared_ptr<Chessman> removePawnsReach  (                                                   );
+
+  // Operators functions
+  Score operator+ (Score) const;
+  Score operator- (Score) const;
+  Score operator+=(Score)      ;
+  Score operator-=(Score)      ;
+
+  // get king id
+  Chessman::ID getKingID() const;
+
+  // Destructor
+  ~User();
+
+  private:
+  // Constructor is private for Singeleton pattern
+  User(Name, Color, Score = 0, Score = 0);
+
+  // Data member
+  // for Singeleton pattern
+  static std::array<User*, 2> users;
+  static int                  cnt  ;
+
+  // Normal Data member
+  Name           name            ;
+  Color          color           ;
+  Score          score           ;
+  Score          negative_score  ;
+  ChessmansIn    chessmans_in    ;
+  ChessmansOut   chessmans_out   ;
+  PawnsReachLast pawns_reach_last;
+
+};
+
+#endif /* end of include guard: USER_H*/
---
+++ src/User.cpp
@@ -0,0 +1,239 @@
+#include "../Header/User.hpp"
+
+#include "../Header/Bishop.hpp"
+#include "../Header/ChessBoard.hpp"
+#include "../Header/King.hpp"
+#include "../Header/Knight.hpp"
+#include "../Header/Pawn.hpp"
+#include "../Header/Queen.hpp"
+#include "../Header/Rook.hpp"
+
+#include <stdexcept>
+#include <QDebug>
+
+
+using namespace std;
+
+std::array<User *, 2> User::users = {nullptr, nullptr};
+int User::cnt = 0;
+
+User*& User::get(Name name, Color color, Score positive_score, Score negative_score)
+{
+    if (cnt < 2)
+        users[cnt++] = new User(name, color, positive_score, negative_score);
+
+    return users[cnt - 1];
+}
+
+User::User(Name name, Color color, Score score, Score negative_score)
+{
+  setName (name );
+  setColor(color);
+  setScore(score);
+  setNegativeScore(negative_score);
+  setChessmansIn();
+}
+
+void User::setName(Name name)
+{
+  this->name = name;
+}
+
+User::Name User::getName() const
+{
+  return name;
+}
+
+void  User::setColor(Color color)
+{
+  this->color = color;
+}
+
+User::Color User::getColor() const
+{
+  return color;
+}
+
+void User::setScore(Score score)
+{
+  this->score = score;
+}
+
+User::Score User::getScore() const
+{
+  return score;
+}
+
+void User::setNegativeScore(Score score)
+{
+  this->negative_score = score;
+}
+
+User::Score User::getNegativeScore() const
+{
+  return negative_score;
+}
+
+User::Score User::incScore(Score score)
+{
+  this->score += score;
+
+  return score;
+}
+
+User::Score User::decScore(Score score)
+{
+  return this->score -= score;
+}
+
+User::Score User::incNegativeScore(Score score)
+{
+  return this->negative_score += score;
+}
+
+User::Score User::operator+(Score score) const
+{
+  return this->score + score;
+}
+
+User::Score User::operator-(Score score) const
+{
+  return this->score - score;
+}
+
+User::Score User::operator+=(Score score)
+{
+  return this->score += score;
+}
+
+User::Score User::operator-=(Score score)
+{
+    return this->score -= score;
+}
+
+void User::setChessmansIn()
+{
+  chessmans_in.push_back(shared_ptr<Chessman>(new Rook  (color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Knight(color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Bishop(color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Queen (color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new King  (color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Bishop(color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Knight(color)));
+  chessmans_in.push_back(shared_ptr<Chessman>(new Rook  (color)));
+
+  for(int i = 8; i < 16; i++)
+  {
+    chessmans_in.push_back(shared_ptr<Chessman>(new Pawn(color)));
+  }
+
+  string temp_color;
+  if(color == Chessman::WHITE)
+    temp_color = "W";
+  else
+    temp_color = "B";
+
+  string file_addres = "qrc:/Assets/Icons/" + temp_color;
+  string names[16] = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
+  for(int i = 8; i < 16; i++)
+    names[i] = "Pawn";
+
+  for(size_t i = 0; i < chessmans_in.size(); i++)
+    chessmans_in[i]->setIcon(file_addres + names[i] + ".png");
+}
+
+User::ChessmansIn User::getChessmansIn() const
+{
+  return chessmans_in;
+}
+
+User::ChessmansOut User::getChessmansOut() const
+{
+  return chessmans_out;
+}
+
+shared_ptr<Chessman> User::getChessman(Chessman::ID id, bool in) const
+{
+  if(in)
+  {
+    for(auto const &item: chessmans_in)
+      if(item->getID() == id)
+        return item;
+  }
+  else
+  {
+    for(auto const &item: chessmans_out)
+      if(item->getID() == id)
+        return item;
+  }
+
+  throw invalid_argument("Any Chessman with this id not existed.");
+}
+
+void User::hitChessman(Chessman::ID id, bool chessman_pawn)
+{
+  for(auto it = chessmans_in.begin(); it != chessmans_in.end(); it++)
+  {
+    if((*it)->getID() == id)
+    {
+      if(chessman_pawn)
+        pawns_reach_last.push_back(*it);
+      else
+        chessmans_out.push_back(*it);
+
+      *it = nullptr;
+      chessmans_in.erase(it);
+      return;
+    }
+  }
+
+  throw invalid_argument("any chessman with this id not founded.");
+}
+
+shared_ptr<Chessman> User::backChessmanInGame(Chessman::ID id)
+{
+  for(auto it = chessmans_out.begin(); it != chessmans_out.end(); it++)
+  {
+    if((*it)->getID() == id)
+    {
+      chessmans_in.push_back(*it);
+      *it = nullptr;
+      chessmans_out.erase(it);
+      return chessmans_in.back();
+    }
+  }
+  throw invalid_argument("Any chessman with this id not founded.");
+}
+
+Chessman::ID User::getKingID() const
+{
+  for(auto const & item: chessmans_in)
+    if(item->getID()[0] == 'K')
+        return item->getID();
+
+  throw runtime_error("King not founded.");
+}
+
+User::~User()
+{
+  cnt--;
+  for(auto & item: chessmans_in)
+    item.reset();
+
+  for(auto & item: chessmans_out)
+    item.reset();
+  users[cnt] = nullptr;
+}
+
+void User::addToChessmansIn(std::shared_ptr<Chessman> chess_man)
+{
+   chessmans_in.push_back(chess_man);
+}
+
+shared_ptr<Chessman> User::removePawnsReach()
+{
+  chessmans_in.push_back(pawns_reach_last.back());
+  pawns_reach_last.pop_back();
+
+  return chessmans_in.back();
+}
