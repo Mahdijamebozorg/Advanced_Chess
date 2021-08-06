205246e878aa5373117900bffbab99f379866540
Amir <sh.hossein67@gmail.com>
2021-08-06 09:55:59 +0430

    add GameManager class to prj, this class mangement game. this class used Singeleton pattern, so can just 1 instance from this class. file: include/GameManger.hpp src/GameManager.cpp

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -1,11 +1,92 @@
 #ifndef GAMEMANAGER_H
 #define GAMEMANAGER_H
 
-#include "Chessman.hpp"
+#include "User.hpp"
 #include "ChessBoard.hpp"
+
+#include <stack>
+
 
 class GameManager
 {
+public:
+  // Type alias
+  using ChessBoardGame = ChessBoard*            ;
+  using GameUser       = User*                  ;
+  using GameName       = std::string            ;
+  using Movements      = std::stack<std::string>;
+  using Enpasan        = Chessman::Index        ;
+
+  // declare enum
+  enum Turn {USER1, USER2};
+
+  // static set and get pointer to this calss functions for Singeleto Pattern
+  static GameManager*& get(GameName name);
+
+  // set and get functions
+  void            setChessBoardGame(const ChessBoardGame&                       )      ;
+  ChessBoardGame  getChessBoardGame(                                            ) const;
+  void            setUser1         (User::Name, User::Score = 0, User::Score = 0)      ;
+  GameUser        getUser1         (                                            ) const;
+  void            setUser2         (User::Name, User::Score = 0, User::Score = 0)      ;
+  GameUser        getUser2         (                                            ) const;
+  void            setTurn          (Turn                                        )      ;
+  Turn            getTurn          (                                            ) const;
+  void            changeTurn       (                                            )      ;
+  void            setGameName      (GameName                                    )      ;
+  GameName        getGameName      (                                            ) const;
+
+  // Enpasan functions
+  void allowEnpasan(Enpasan);
+  void notAllowEnpasan();
+
+  // CanGo functions
+  std::pair<std::vector<Chessman::Index>, std::vector<Chessman::Index>> getCellState(Chessman::Index) const;
+
+  // Move and hit chessman
+  void movePiece(Chessman::Index src, Chessman::Index dest, bool move = false); // if(move) order move to movements
+
+  // Convert order to string   // string order: "Chessman(ID) Xi Xi Chessman(ID) or N"
+  std::string convertOrderToString(Chessman::Index src, Chessman::Index dest);
+
+  // convert Index string to index int
+  Chessman::Index                             convertIndexStringToIndexInt(std::string index) const;
+  std::pair<Chessman::Index, Chessman::Index> getLastMove(                                  )      ;
+
+  // Undo
+  std::pair<Chessman::Index, Chessman::Index> undo();
+
+  // Start, End and Restart Game Functions
+  void startGame();
+  void restartGame();
+  void endGame();
+
+  // Random and Extra Movement functions
+  std::pair<bool, std::pair<Chessman::Index, Chessman::Index>> randomMovements();
+  bool extraMovements();
+
+  // checked kish user
+  bool isKingChecked();
+
+  // Destructor
+  ~GameManager();
+
+  // When Pawn reach to final cell
+  void promote(Chessman::Index, Chessman::ChessType);
+
+  private:
+  // Construcotr is private for Singeleton Pattern
+  GameManager(GameName          )         ;
+  GameManager(const GameManager&) = delete; // Copy constructor
+
+  // Data member
+  static GameManager*     game_manager                            ;
+  std::array<GameUser, 2> users         = {nullptr, nullptr}      ;
+  ChessBoardGame          chess_board   = nullptr                 ;
+  Turn                    turn          = USER1                   ;
+  GameName                game_name                               ;
+  Movements               movements     = {}                      ;
+  Enpasan                 enpasan       = std::make_pair(100, 100);
 
 };
 
---
+++ src/GameManager.cpp
@@ -0,0 +1,520 @@
+#include "../Header/GameManager.hpp"
+
+#include "../Header/Bishop.hpp"
+#include "../Header/Exceptions.hpp"
+#include "../Header/King.hpp"
+#include "../Header/Knight.hpp"
+#include "../Header/Pawn.hpp"
+#include "../Header/Queen.hpp"
+#include "../Header/Rook.hpp"
+#include "../Header/User.hpp"
+#include <ctime>
+#include <random>
+#include <stdexcept>
+
+#include <QDebug>
+
+using namespace std;
+
+GameManager *GameManager::game_manager = nullptr;
+
+GameManager*& GameManager::get(GameName name)
+{
+    if (game_manager == nullptr)
+        game_manager = new GameManager(name);
+
+    return game_manager;
+}
+
+GameManager::GameManager(GameName game_name)
+{
+    setGameName(game_name);
+}
+
+GameManager::~GameManager()
+{
+  if(users[0] != nullptr)
+  {
+    delete users[0];
+    users[0] = nullptr;
+  }
+  if(users[1] != nullptr)
+  {
+    delete users[1];
+    users[1] = nullptr;
+  }
+
+  if(chess_board != nullptr)
+  {
+    delete chess_board;
+    chess_board = nullptr;
+  }
+
+  chess_board = nullptr;
+}
+
+void GameManager::startGame()
+{
+  if(users[0] == nullptr || users[1] == nullptr)
+    throw runtime_error("All user not seted.");
+
+  setChessBoardGame(ChessBoard::get(users[0], users[1]));
+}
+
+pair<vector<Chessman::Index>, vector<Chessman::Index>> GameManager::getCellState(Chessman::Index index) const
+{
+  if(chess_board->getCell(index).getChessPieces() == nullptr) // Check in this home has chessman
+      throw EmptySquare("In this home any chessman not founded.");
+
+  if(turn == USER1)
+  {
+    if(chess_board->getCell(index).getChessPieces()->getColor() == Chessman::BLACK)
+      throw AccessDenied("User1 can't selected Black Chessman");
+  }
+  else
+  {
+    if(chess_board->getCell(index).getChessPieces()->getColor() == Chessman::WHITE)
+      throw AccessDenied("User2 can't selected White Chessman");
+  }
+
+  return chess_board->getCanGo(index, true, enpasan);
+}
+
+void GameManager::movePiece(Chessman::Index src, Chessman::Index dest, bool in_undo)
+{
+  if(src != dest)
+  {
+    if(chess_board->getCell(dest).isFull())
+      if(chess_board->getCell(dest).getChessPieces()->getChessType() == Chessman::KING)
+        throw ImpossibleHitKing("King Could'nt be hited");
+
+    if((src.first == 1 && dest.first == 3) || (src.first == 6 && dest.first == 4))
+    {
+      if(chess_board->getCell(src).getChessPieces()->getChessType() == Chessman::PAWN)
+         this->allowEnpasan(dest);
+    }
+    else
+    {
+      this->notAllowEnpasan();
+    }
+
+    if(chess_board->getCell(src).isFull())
+    {
+      if(chess_board->getCell(src).getChessPieces()->getChessType() == Chessman::KING && in_undo == false)
+      {
+        if(dest.second - src.second == 2)
+          movePiece(make_pair(dest.first, dest.second + 1), make_pair(dest.first, dest.second - 1), in_undo);
+        else if(src.second - dest.second == 2)
+          movePiece(make_pair(dest.first, dest.second - 2), make_pair(dest.first, dest.second + 1), in_undo);
+      }
+    }
+
+    if(in_undo == false)
+      movements.push(convertOrderToString(src, dest));
+
+    pair<Chessman::ID, Chessman::ID> temp = chess_board->moveChessman(src, dest);
+
+    if(temp.first != "")
+    {
+      if(turn == USER1) // move chessman from chessmans_in to chessmans_out in user class
+        users[1]->hitChessman(temp.first);
+      else
+        users[0]->hitChessman(temp.first);
+    }
+
+    if(temp.second != "")
+    {
+      string temp_movement = "ENP ";
+      if(turn == USER1) // move chessman from chessmans_in to chessmans_out in user class
+      {
+        temp_movement += static_cast<char>(97 + dest.first + 1);
+        users[1]->hitChessman(temp.second);
+      }
+      else
+      {
+        temp_movement += static_cast<char>(97 + dest.first - 1);
+        users[0]->hitChessman(temp.second);
+      }
+
+      temp_movement += to_string(dest.second) + temp.second;
+      movements.push(temp_movement);
+    }
+
+    // if chessman that moved is king or rook moved for it chessman inserted true
+    shared_ptr<Chessman> temp_chess = chess_board->getCell(dest).getChessPieces();
+    if(temp_chess->getChessType() == Chessman::KING)
+      dynamic_cast<King*>(temp_chess.get())->kingMoved();
+    else if(temp_chess->getChessType() == Chessman::ROOK)
+      dynamic_cast<Rook*>(temp_chess.get())->rookMoved();
+
+    if(temp_chess->getChessType() == Chessman::KING && in_undo == true)
+    {
+      if(dest.second - src.second == 2)
+      {
+        this->undo();
+        dynamic_cast<King*>(temp_chess.get())->kingNotMoved();
+        dynamic_cast<Rook*>(chess_board->getCell(make_pair(src.first, src.second - 2)).getChessPieces().get())->rookNotMoved();
+        changeTurn();
+      }
+      else if(src.second - dest.second == 2)
+      {
+        this->undo();
+        dynamic_cast<King*>(temp_chess.get())->kingNotMoved();
+        dynamic_cast<Rook*>(chess_board->getCell(make_pair(src.first, src.second + 1)).getChessPieces().get())->rookNotMoved();
+        changeTurn();
+      }
+    }
+    if(dest.first == 0 || dest.first == 7)
+      if(chess_board->getCell(dest).getChessPieces()->getChessType() == Chessman::ChessType::PAWN)
+        throw FinalCellForPawn("Pawn moved to final cell");
+  }
+}
+
+void GameManager::setUser1(User::Name user1_name, User::Score user1_Pscore, User::Score user1_Nscore)
+{
+    this->users[0] =  User::get(user1_name, User::Color::WHITE, user1_Pscore, user1_Nscore);
+}
+
+void GameManager::setUser2(User::Name user2_name, User::Score user2_Pscore, User::Score user2_Nscore)
+{
+    this->users[1] = User::get(user2_name, User::Color::BLACK, user2_Pscore, user2_Nscore);
+}
+
+GameManager::GameUser GameManager::getUser1() const
+{
+  return users[0];
+}
+
+GameManager::GameUser GameManager::getUser2() const
+{
+  return users[1];
+}
+
+void GameManager::setChessBoardGame(const ChessBoardGame& chess_board)
+{
+  this->chess_board = chess_board;
+}
+
+GameManager::ChessBoardGame GameManager::getChessBoardGame() const
+{
+  return chess_board;
+}
+
+void GameManager::changeTurn()
+{
+  if(getTurn() == USER1)
+    setTurn(USER2);
+  else
+    setTurn(USER1);
+}
+
+void GameManager::setTurn(Turn turn)
+{
+  this->turn = turn;
+}
+
+GameManager::Turn GameManager::getTurn() const
+{
+  return turn;
+}
+
+void GameManager::setGameName(GameName game_name)
+{
+  this->game_name = game_name;
+}
+
+GameManager::GameName GameManager::getGameName() const
+{
+  return game_name;
+}
+
+string GameManager::convertOrderToString(Chessman::Index src, Chessman::Index dest)
+{
+  shared_ptr<Chessman> temp_chessman = chess_board->getCell(src).getChessPieces();
+  if(temp_chessman == nullptr)
+    throw invalid_argument("any chessman not exisetd in this cell.");
+
+  string temp = temp_chessman->getID() + " ";
+  temp += ((char)(97 + src.first ) + to_string(src.second ));
+  temp += ((char)(97 + dest.first) + to_string(dest.second));
+
+  temp_chessman = chess_board->getCell(dest).getChessPieces();
+  if(temp_chessman != nullptr)
+    temp += temp_chessman->getID();
+  else
+    temp += "N";
+
+  qDebug() << "qDebug: " << temp.c_str() << endl;
+  return temp;
+}
+
+pair<Chessman::Index, Chessman::Index> GameManager::undo()
+{
+  if(movements.empty())
+    return make_pair(Chessman::Index(-1, -1), Chessman::Index(-1, -1));
+
+  string temp = movements.top();
+  movements.pop();
+
+  bool temp_enpasan = false;
+  Chessman::Index temp_src;
+  if(temp.substr(0, 3) == "PRF") // promote
+  {
+    changeTurn();
+    size_t pos = temp.find(" ");
+    temp_src   = convertIndexStringToIndexInt(temp.substr(pos + 1, 2));
+    chess_board->hitChessman(temp_src);
+
+    unsigned int temp_turn;
+    if(turn == USER1)
+      temp_turn = 0;
+    else
+      temp_turn = 1;
+
+    chess_board->getCell(temp_src).setChessPieces(users[temp_turn]->removePawnsReach());
+
+    temp = movements.top();
+    movements.pop();
+
+    changeTurn();
+  }
+  else if(temp.substr(0, 3) == "ENP") // Enpasan
+  {
+    temp_enpasan = true;
+    temp_src = convertIndexStringToIndexInt(temp.substr(4, 2));
+    Chessman::ID temp_id  = temp.substr(6)                                 ;
+
+    unsigned int temp_turn;
+    if(turn == USER1)
+    {
+      temp_turn = 0;
+      chess_board->addChessmanIndex(Chessman::WHITE, temp_src);
+    }
+    else
+    {
+      temp_turn = 1;
+      chess_board->addChessmanIndex(Chessman::BLACK, temp_src);
+    }
+
+    chess_board->getCell(temp_src).setChessPieces(users[temp_turn]->backChessmanInGame(temp_id));
+
+    temp = movements.top();
+    movements.pop();
+  }
+
+  size_t pos = temp.find(" ");
+  Chessman::Index src  = convertIndexStringToIndexInt(temp.substr(pos + 1, 2));
+  Chessman::Index dest = convertIndexStringToIndexInt(temp.substr(pos + 3 , 2));
+  this->movePiece(dest, src, true);
+
+  if(temp.back() != 'N')
+  {
+    shared_ptr<Chessman> temp_chessman;
+    if (turn == USER2)
+    {
+      temp_chessman = users[1]->getChessman(temp.substr(pos + 5), false);
+      users[1]->backChessmanInGame(temp.substr(pos + 5));
+    }
+    else
+    {
+      temp_chessman = users[0]->getChessman(temp.substr(pos + 5), false);
+      users[0]->backChessmanInGame(temp.substr(pos + 5));
+    }
+
+    this->chess_board->getCell(dest).setChessPieces(temp_chessman);
+    chess_board->addChessmanIndex(temp_chessman->getColor(), dest);
+  }
+
+  this->changeTurn();
+
+  if(turn == USER1)
+      this->users[0]->decScore(5);
+  else
+    this->users[1]->decScore(5);
+
+  if(temp_enpasan)
+    allowEnpasan(temp_src);
+
+  return make_pair(src, dest);
+}
+
+Chessman::Index GameManager::convertIndexStringToIndexInt(string index) const
+{
+  int j = 0;
+  for(char ch = 'a'; ch <= 'h'; ch++, j++)
+  {
+    if(ch == index[0])
+      return make_pair(j, int(index[1]) - 48);
+  }
+  throw invalid_argument("string must have 2 charachter and char #0 must between a, h");
+}
+
+std::pair<Chessman::Index, Chessman::Index> GameManager::getLastMove()
+{
+  if(movements.size() == 0)
+    throw EmptyMoveMents("move ments is empty");
+
+  if(movements.top().substr(0, 3) == "PRF")
+      movements.pop();
+
+  string temp = movements.top();
+  int pos = temp.find(" ");
+  string src = temp.substr(pos + 1, 2);
+  string dest = temp.substr(pos + 3, 2);
+
+  return make_pair(convertIndexStringToIndexInt(src), convertIndexStringToIndexInt(dest));
+}
+
+void GameManager::restartGame()
+{
+  string temp_name1 = users[0]->getName();
+  string temp_name2 = users[1]->getName();
+
+  delete users[0];
+  users[0] = nullptr;
+  delete users[1];
+  users[1] = nullptr;
+
+  delete chess_board;
+  chess_board = nullptr;
+
+  setUser1(temp_name1);
+  setUser2(temp_name2);
+
+  this->turn = USER1;
+  size_t size = this->movements.size();
+  for(size_t i = 0; i < size; i++)
+    movements.pop();
+
+  startGame();
+}
+
+void GameManager::endGame()
+{
+  delete users[0];
+  users[0] = nullptr;
+  delete users[1];
+  users[1] = nullptr;
+
+  delete chess_board;
+  chess_board = nullptr;
+
+  this->turn = USER1;
+  size_t size = this->movements.size();
+  for(size_t i = 0; i < size; i++)
+    movements.pop();
+}
+
+pair<bool, pair<Chessman::Index, Chessman::Index>> GameManager::randomMovements()
+{
+  int temp_turn;
+  if(turn == USER1)
+    temp_turn = 0;
+  else
+    temp_turn = 1;
+
+  srand(time(nullptr));
+
+  vector<shared_ptr<Chessman>> temp_chessmans = users[temp_turn]->getChessmansIn();
+
+  Chessman::ID temp_id = temp_chessmans[rand() % temp_chessmans.size()]->getID();
+
+  Chessman::Index temp_src = chess_board->getIndex(temp_id);
+
+  vector<Chessman::Index> temp_can_go = getCellState(temp_src).first;
+  if(temp_can_go.size() == 0)
+    return make_pair(false, make_pair(make_pair(100, 100), make_pair(100, 100)));
+
+  Chessman::Index temp_dest = temp_can_go[rand() % temp_can_go.size()];
+
+  movePiece(temp_src, temp_dest);
+
+  return make_pair(true, make_pair(temp_src, temp_dest));
+}
+
+bool GameManager::extraMovements()
+{
+  int temp;
+  if(turn == USER1)
+    temp = 0;
+  else
+    temp = 1;
+
+  if(users[temp]->getScore() >= 30)
+  {
+    users[temp]->decScore(30);
+    return true;
+  }
+
+  return false;
+}
+
+bool GameManager::isKingChecked()
+{
+  if(turn == USER1)
+    return chess_board->isChecked(chess_board->getIndex(users[0]->getKingID()), Chessman::BLACK).first;
+
+  return chess_board->isChecked(chess_board->getIndex(users[1]->getKingID()), Chessman::BLACK).first;
+}
+
+void GameManager::promote(Chessman::Index index, Chessman::ChessType chess_type)
+{
+  Chessman::Icon  temp_icon = "qrc:/Assets/Icons/";
+  Chessman::Color temp_color;
+  unsigned int temp;
+  if(turn == USER1)
+  {
+    temp = 0;
+    temp_color = Chessman::Color::WHITE;
+    temp_icon += "W";
+  }
+  else
+  {
+    temp = 1;
+    temp_color = Chessman::Color::BLACK;
+    temp_icon += "B";
+  }
+
+  users[temp]->hitChessman(chess_board->getCell(index).getChessPieces()->getID(), true);
+  string temp_name;
+  switch(chess_type)
+  {
+    case Chessman::KNIGHT:
+         chess_board->getCell(index).setChessPieces(shared_ptr<Chessman>(new Knight(temp_color, temp_icon + "Knight.png")));
+         temp_name = "H";
+      break;
+
+    case Chessman::BISHOP:
+         chess_board->getCell(index).setChessPieces(shared_ptr<Chessman>(new Bishop(temp_color, temp_icon + "Bishop.png")));
+         temp_name = "B";
+      break;
+
+    case Chessman::ROOK:
+         chess_board->getCell(index).setChessPieces(shared_ptr<Chessman>(new Rook(temp_color, temp_icon + "Rook.png")));
+         temp_name = "R";
+      break;
+
+    case Chessman::QUEEN:
+         chess_board->getCell(index).setChessPieces(shared_ptr<Chessman>(new Queen(temp_color, temp_icon + "Queen.png")));
+         temp_name = "Q";
+      break;
+
+    default:
+        throw invalid_argument("This ChessType not allow.");
+  }
+
+  users[temp]->addToChessmansIn(chess_board->getCell(index).getChessPieces());
+
+  movements.push("PRF" + chess_board->getCell(index).getChessPieces()->getID() + " " +
+                 ((char)(97 + index.first)) + to_string(index.second));
+}
+
+void GameManager::allowEnpasan(Enpasan enpasan)
+{
+  this->enpasan = enpasan;
+}
+
+void GameManager::notAllowEnpasan()
+{
+  enpasan = make_pair(100, 100);
+}
